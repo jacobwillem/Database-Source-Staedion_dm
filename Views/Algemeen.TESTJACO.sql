4ee8d1150a7d91c872aa60eb18c4b4a5000881a8
jacobwillem <44579942+jacobwillem@users.noreply.github.com>
2021-09-04 10:51:47 +0200

    Gaat naar lokaal toe ? Niet naar github ?

--- Views/Algemeen.TESTJACO.sql
+++ Views/Algemeen.TESTJACO.sql
@@ -2,5 +2,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-create view [Algemeen].[TESTJACO] as select top 10 * from empire_dwh.dbo.tijd go
+
+CREATE view [Algemeen].[TESTJACO] as 
+-- documentatie toevoegen
+
+select top 10 * from empire_dwh.dbo.tijd go
 GO
