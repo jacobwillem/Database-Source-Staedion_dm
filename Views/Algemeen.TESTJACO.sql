9d1d1a5b7f92073280d38ba88c60a5042cd4ca5b
jacobwillem <44579942+jacobwillem@users.noreply.github.com>
2021-09-10 11:37:48 +0200

    Test aanpassing view

--- Views/Algemeen.TESTJACO.sql
+++ Views/Algemeen.TESTJACO.sql
@@ -4,9 +4,11 @@ SET ANSI_NULLS ON
 GO
 
 
+
 CREATE view [Algemeen].[TESTJACO] as 
 -- documentatie toevoegen
 -- tweede aanpassing
+-- derde aanpassing
 
 select top 10 * from empire_dwh.dbo.tijd go
 GO
